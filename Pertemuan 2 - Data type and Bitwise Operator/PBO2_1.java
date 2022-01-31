f094ad5df285a24305da55230a41277b23232a3e
Roihan Kusuma Wardana <79994773+ZRX1204@users.noreply.github.com>
2022-01-31 23:02:46 +0700

    PBO sesi 2

---
+++ Pertemuan 2 - Data type and Bitwise Operator/PBO2_1.java
@@ -0,0 +1,20 @@
+/*
+ * Nama    : Roihan Kusuma Wardana
+ * NIM     : 20200040049
+ * Kelas   : TI20A
+ */
+package pbo2;
+
+/**
+ *
+ * @author ZRX1204
+ */
+public class PBO2_1 {
+    public static void main(String[] args) {
+        int uang = 50000;
+        int beli = 55000;
+        String result = uang > beli?"jadi membeli":"Tidak jadi membeli";
+        System.out.println(result);
+    }
+    
+}
