3be194ea1bb038e63da653919dcadbe564d29e2b
Roihan Kusuma Wardana <79994773+ZRX1204@users.noreply.github.com>
2022-01-31 23:04:12 +0700

    PBO sesi 1

---
+++ Pertemuan 1 - Indtroduction/PBO1_1.java
@@ -0,0 +1,19 @@
+/*
+ * Nama     : Roihan Kusuma Wardana
+ * NIM      : 20200040049
+ * Kelas    : TI20A
+ */
+package pbo1;
+
+/**
+ *
+ * @author ZRX1204
+ */
+public class PBO1_1 {
+    public static void main(String[] args) {
+        System.out.println("Belajar JAVA\n");
+        System.out.println("Sangat mudah sekali\n");
+        System.out.println("dan sangat menyenangkan\n");
+    }
+    
+}
---
+++ Pertemuan 1 - Indtroduction/PBO1_2.java
@@ -0,0 +1,18 @@
+/*
+ * Nama    : Roihan Kusuma Wardana
+ * NIM     : 20200040049
+ * Kelas   : TI20A
+ */
+package pbo1;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ZRX1204
+ */
+public class PBO1_2 {
+    public static void main(String args[]) {
+        String masukan = JOptionPane.showInputDialog("Anda sedang belajar apa? ");
+        JOptionPane.showMessageDialog(null, "Belajar "+masukan+" Sangat mudah   ");
+    }
+}
